0210699113bae6d9ad235c79cfac2fde54f54e27
dufferprog <30566526+dufferprog@users.noreply.github.com>
2017-09-03 06:01:21 -0400

    Add files via upload

--- cmd/m_all.cmd
+++ cmd/m_all.cmd
@@ -1,2 +1,13 @@
+@:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@::
+@::  m_all -- make both verbex_base and verbex_imports
+@::
+@:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 nmake /nologo /fE:\Data01\program\verbexx\mk\verbexx_base.mk 
+
+@IF %ERRORLEVEL% NEQ 0 ( GOTO end )
+
 nmake /nologo /fE:\Data01\program\verbexx\mk\verbexx_imports.mk 
+
+:end
